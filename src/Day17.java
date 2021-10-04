d27539438413e5b872e5c3214ffd06309cadfca9
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2021-10-04 09:03:07 +0530

    Create Day17.java

---
+++ src/Day17.java
@@ -0,0 +1,173 @@
+/* add matrix */
+public class MatrixAdditionExample{  
+public static void main(String args[]){  
+//creating two matrices    
+int a[][]={{1,3,4},{2,4,3},{3,4,5}};    
+int b[][]={{1,3,4},{2,4,3},{1,2,4}};    
+    
+//creating another matrix to store the sum of two matrices    
+int c[][]=new int[3][3];  //3 rows and 3 columns  
+    
+//adding and printing addition of 2 matrices    
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+c[i][j]=a[i][j]+b[i][j];    //use - for subtraction  
+System.out.print(c[i][j]+" ");    
+}    
+System.out.println();//new line    
+}    
+}}  
+
+/* substract two matrix */
+public class Sub_Matrix  
+{  
+    public static void main(String[] args) {  
+        int rows, cols;  
+  
+        //Initialize matrix a  
+          int a[][] = {  
+                          {4, 5, 6},  
+                          {3, 4, 1},  
+                          {1, 2, 3}  
+                       };  
+  
+          //Initialize matrix b  
+          int b[][] = {  
+                          {2, 0, 3},  
+                         {2, 3, 1},  
+                         {1, 1, 1}  
+                     };  
+  
+          //Calculates number of rows and columns present in given matrix  
+          rows = a.length;  
+        cols = a[0].length;  
+  
+          //Array diff will hold the result  
+        int diff[][] = new int[rows][cols];  
+  
+        //Performs subtraction of matrices a and b. Store the result in matrix diff  
+        for(int i = 0; i < rows; i++){  
+            for(int j = 0; j < cols; j++){  
+                diff[i][j] = a[i][j] - b[i][j];  
+            }  
+        }  
+  
+        System.out.println("Subtraction of two matrices: ");  
+        for(int i = 0; i < rows; i++){  
+            for(int j = 0; j < cols; j++){  
+               System.out.print(diff[i][j] + " ");  
+            }  
+            System.out.println();  
+        }  
+    }  
+} 
+
+/* Multiply of two matrix */
+public class MatrixMultiplicationExample{  
+public static void main(String args[]){  
+//creating two matrices    
+int a[][]={{1,1,1},{2,2,2},{3,3,3}};    
+int b[][]={{1,1,1},{2,2,2},{3,3,3}};    
+    
+//creating another matrix to store the multiplication of two matrices    
+int c[][]=new int[3][3];  //3 rows and 3 columns  
+    
+//multiplying and printing multiplication of 2 matrices    
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+c[i][j]=0;      
+for(int k=0;k<3;k++)      
+{      
+c[i][j]+=a[i][k]*b[k][j];      
+}//end of k loop  
+System.out.print(c[i][j]+" ");  //printing matrix element  
+}//end of j loop  
+System.out.println();//new line    
+}    
+}}  
+
+/* check the two matrix are equal or not */
+public class EqualMatrix    
+{    
+    public static void main(String[] args) {    
+        int row1, col1, row2, col2;    
+        boolean flag = true;    
+            
+        //Initialize matrix a    
+        int a[][] = {       
+                        {1, 2, 3},    
+                        {8, 4, 6},    
+                        {4, 5, 7}    
+                    };    
+              
+          //Initialize matrix b    
+        int b[][] = {       
+                        {1, 2, 3},    
+                        {8, 4, 6},    
+                        {4, 5, 7}    
+            };    
+              
+        //Calculates the number of rows and columns present in the first matrix    
+    
+          row1 = a.length;    
+        col1 = a[0].length;    
+            
+        //Calculates the number of rows and columns present in the second matrix    
+    
+          row2 = b.length;    
+        col2 = b[0].length;    
+            
+        //Checks if dimensions of both the matrices are equal    
+        if(row1 != row2 || col1 != col2){    
+            System.out.println("Matrices are not equal");    
+        }    
+        else {    
+            for(int i = 0; i < row1; i++){    
+                for(int j = 0; j < col1; j++){    
+                  if(a[i][j] != b[i][j]){    
+                      flag = false;    
+                      break;    
+                  }    
+                }    
+            }    
+                
+            if(flag)    
+                System.out.println("Matrices are equal");    
+            else    
+                System.out.println("Matrices are not equal");    
+        }    
+    }    
+} 
+
+
+/* transponce of matrix */
+public class MatrixTransposeExample{  
+public static void main(String args[]){  
+//creating a matrix  
+int original[][]={{1,3,4},{2,4,3},{3,4,5}};    
+    
+//creating another matrix to store transpose of a matrix  
+int transpose[][]=new int[3][3];  //3 rows and 3 columns  
+    
+//Code to transpose a matrix  
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+transpose[i][j]=original[j][i];  
+}    
+}    
+  
+System.out.println("Printing Matrix without transpose:");  
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+System.out.print(original[i][j]+" ");    
+}    
+System.out.println();//new line    
+}    
+System.out.println("Printing Matrix After Transpose:");  
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+System.out.print(transpose[i][j]+" ");    
+}    
+System.out.println();//new line    
+}    
+}} 
